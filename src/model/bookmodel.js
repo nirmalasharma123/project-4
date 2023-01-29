5819467b20fbee76784977eaf1bc269855993305
nirmalasharma123 <119753743+nirmalasharma123@users.noreply.github.com>
2023-01-29 14:55:57 +0530

    Update bookmodel.js

--- src/model/bookmodel.js
+++ src/model/bookmodel.js
@@ -58,17 +58,3 @@ const bookSchema = new mongoose.Schema(
 
 module.exports=mongoose.model('book',bookSchema);
  
-//  {
-//     title: {string, mandatory, unique},
-//     excerpt: {string, mandatory},
-//     userId: {ObjectId, mandatory, refs to user model},
-//     ISBN: {string, mandatory, unique},
-//     category: {string, mandatory},
-//     subcategory: {string, mandatory},
-//     reviews: {number, default: 0, comment: Holds number of reviews of this book},
-//     deletedAt: {Date, when the document is deleted},
-//     isDeleted: {boolean, default: false},
-//     releasedAt: {Date, mandatory, format("YYYY-MM-DD")},
-//     createdAt: {timestamp},
-//     updatedAt: {timestamp},
-//   }
