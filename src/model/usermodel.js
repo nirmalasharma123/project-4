c08a75ac8305a1f36c8647dde92f5439de6ee2c6
nirmalasharma123 <119753743+nirmalasharma123@users.noreply.github.com>
2023-01-29 14:57:41 +0530

    Update usermodel.js

--- src/model/usermodel.js
+++ src/model/usermodel.js
@@ -9,22 +9,30 @@ const userSchema= new mongoose.Schema({
     },
     name: {
         type:String,
+        lowercase:true,
          required:true,
          trim:true
         },
     phone: {
         type:String,
-         required:true, 
+        required:true, 
+        trim:true,
+        unique:true
         
         },
     email: {
         type:String, 
-        required:true, 
+        required:true,
+        trim:true, 
+        unique:true
         
     } ,
     password: {
         type:String, 
-        required:true },
+        required:true,
+        min:8,
+        max:15
+    },
     address: {
        street: {type:String},
       city: {type:String},
@@ -35,17 +43,3 @@ const userSchema= new mongoose.Schema({
 
     module.exports=mongoose.model('usermodel',userSchema);
 
-// { 
-//     title: {string, mandatory, enum[Mr, Mrs, Miss]},
-//     name: {string, mandatory},
-//     phone: {string, mandatory, unique},
-//     email: {string, mandatory, valid email, unique}, 
-//     password: {string, mandatory, minLen 8, maxLen 15},
-//     address: {
-//       street: {string},
-//       city: {string},
-//       pincode: {string}
-//     },
-//     createdAt: {timestamp},
-//     updatedAt: {timestamp}
-//   }
